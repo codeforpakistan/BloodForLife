2c184c170156c94489f5d49c7c94f0efc11ab10e
Shakeel <shakeel@redbuffer.net>
2014-01-25 20:06:40 +0500

    log

--- bfl.js
+++ bfl.js
@@ -60,7 +60,7 @@ if (Meteor.isServer) {
 				var curr_date = d.getDate();
 				var curr_month = d.getMonth();
 				var curr_year = d.getFullYear();
-				console.log(donor.find({blood_group:q[0] , city : q[1], last_bleed : { $lte : gtcurr_date + "-" + curr_month + "-" + curr_year }}).fetch() );
+				console.log(donor.find({blood_group:q[0] , city : q[1], last_bleed : { $lte : curr_date + "-" + curr_month + "-" + curr_year }}).fetch() );
 				var xml = '<Response><Sms>Thank you for submitting your question!</Sms></Response>';
 				return [200, {"Content-Type": "text/xml"}, xml];
 		});	
