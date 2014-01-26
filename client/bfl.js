15d9bb18d4ac15c8c2418cd8822aaf0a1311ca49
Shakeel <shakeel@redbuffer.net>
2014-01-26 10:18:28 +0500

    new struct

---
+++ client/bfl.js
@@ -0,0 +1,113 @@
+sms = new Meteor.Collection("requests");
+donor = new Meteor.Collection("donor");
+
+Meteor.subscribe("requests");
+Meteor.subscribe("donor");
+
+
+Meteor.Router.add({
+	'/search': function(){
+		var query = this.querystring.split("&");
+		if(query.length >= 2){
+			var city = query[1].split("=");
+			var group = query[0].split("=");
+			Session.set('results', donor.find({blood_group:group[1], city:city[1]}).fetch());
+		}else{
+			Session.set('results',null);
+		}
+		return 'search';	
+	},
+	'/' : function(){
+		return 'home';
+	},
+	'/login': function() {
+		return 'login';
+	},
+	'/register': function() {
+		return 'register';
+	},
+	'*': 'not_found'
+});
+
+Template.search.events({
+	'click #search' : function(e,t){
+		e.preventDefault();
+		Meteor.Router.to('/search?group='+$("#group option:selected").val()+"&city="+$("#city option:selected").val());
+	}
+});
+Template.search.helpers({
+	result : function() {
+		return Session.get('results');
+	}
+});
+Template.home.helpers({
+	notlogggedIn : function() {
+		if(Meteor.user() == null){
+			return 1;
+		}
+	},
+	loggedIn : function(){
+		if(Meteor.user() != null){
+			return 1;
+		}
+	}
+});
+Template.search.rendered = function(){
+	$('table').dataTable(); 
+};
+Template.register.events({
+	'click #register' : function(e,t){
+		e.preventDefault();
+		var email = t.find('#email').value
+			, password = t.find('#password').value;
+
+		var Donor = {
+			blood_group : t.find('#blood_group option:selected').value,
+			title : t.find('#title option:selected').value,
+			full_name : t.find('#name').value,
+			gender : t.find('#sex input[type=radio]:checked').value,
+			dob : t.find('#dob').value,
+			email : t.find('#email').value,
+			cell : t.find('#cell').value,
+			phone : t.find('#home').value,
+			address : t.find('#address').value,
+			city : t.find('#city option:selected').value,
+		}
+		console.log(Donor);
+		var uid = donor.insert(Donor);
+		//Trim and validate the input
+
+			Accounts.createUser({email: email, password : password, profile : {did: uid, name: full_name}}, function(err){
+				if (err) {
+					// Inform the user that account creation failed
+				} else {
+					// Success. Account has been created and the user
+					// has logged in successfully. 
+					Meteor.Router.to('/');
+				}
+
+			});
+			return false;
+	}
+});
+Handlebars.registerHelper("calcAge", function (dateString, options) {
+	var birthday = +new Date(dateString);
+	return humanise(~~((Date.now() - birthday) / (86400000)));
+});
+function humanise (diff) {
+	var str = '';
+	var values = {
+		' year': 365, 
+		' month': 30, 
+		' day': 1
+	};
+	for (var x in values) {
+			var amount = Math.floor(diff / values[x]);
+			if (amount >= 1) {
+				str += amount + x + (amount > 1 ? 's' : '') + ' ';
+				diff -= amount * values[x];
+			}
+		}
+		return str;
+	};
+
